ec63778a0c5d6ac9c617d248ca762af39f0dd6b0
Luiz Eduardo Martins <led.martins93@gmail.com>
2022-12-10 14:26:44 +0000

    Update dependencies and fix spring boot context

--- src/test/java/org/example/ApiApplicationTest.java
+++ src/test/java/org/example/ApiApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ApiTest {
+public class ApiApplicationTest {
 
     @Test
     void contextLoads() {
